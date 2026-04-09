ec60857393e13ed2342744c1969341566237ccfa
AntonDmitriev1484 <a.dmitriev@wustl.edu>
2026-04-09 11:31:03 -0500

    Updated source command

--- start_opti_node.sh
+++ start_opti_node.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-source /home/admi3ev/motion_capture_tracking/install/setup.bash
+source /home/admi3ev/ros2_ws/install/setup.bash
 
 ros2 launch motion_capture_tracking launch.py
